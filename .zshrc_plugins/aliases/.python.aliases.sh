01d1c4bfefefb527acd93571a894f0ab2e9abde2
Jay Miller <kjaymiller@gmail.com>
2024-08-27 20:55:36 -0400

    rename sva to pva

--- .zshrc_plugins/aliases/.python.aliases.sh
+++ .zshrc_plugins/aliases/.python.aliases.sh
@@ -1,5 +1,5 @@
 alias py=python
-alias sva="source .venv/bin/activate"
+alias pva="source .venv/bin/activate"
 alias pv.v="python -m venv .venv"
 alias puppip="python -m pip install --upgrade pip"
 alias piptc="python -m pip install piptools compile"
