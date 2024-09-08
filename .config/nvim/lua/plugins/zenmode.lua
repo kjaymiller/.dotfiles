1a12c80778335241d857afaa65bfc2b7b2155ab6
Jay Miller <kjaymiller@gmail.com>
2024-09-08 11:50:02 -0400

    add twilight and zenmode

---
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,14 @@
+return {
+    "folke/zen-mode.nvim",
+    opts = {
+        wezterm = {
+            enabled = false,
+            -- can be either an absolute font size or the number of incremental steps
+            font = "+4", -- (10% increase per step)
+        },
+        tmux = { enabled = false },
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+}
