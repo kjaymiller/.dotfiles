89f1c4e741a7ebfea5e43db329dddd7c9fb88bc1
Jay Miller <kjaymiller@gmail.com>
2024-09-08 11:50:26 -0400

    add gh aliases

---
+++ .zshrc_plugins/aliases/.gh.aliases.sh
@@ -0,0 +1,10 @@
+# Aliases for the Github CLI
+
+# GH Issues
+alias ghil="gh issue list"
+alias ghils="gh issue list --json number --json title"
+alias ghiv="gh issue view"
+alias ghiq="gh issue --json number --json title --json labels --jq"
+alias ghis="gh issue --search"
+alias ghid="gh issue develop -c"
+alias ghix="gh issue delete"
